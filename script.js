2443df1d7e3b89860c58cb2a3cf3163a239d0a7f
Jayeshrakhonde07 <jayeshrakhonde05@gmail.com>
2026-08-15 13:29:06 +0530

    modified songs

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let songIndex = 0;
 
 let songs = [
-    { songName:"Shri Hanuman Chalisa", filePath:"/songs/Song1.mp3", coverPath:"/covers/Cover1.jpeg" },
-    { songName:"Dhrundhar Title Song", filePath:"/songs/Song2.mp3", coverPath:"/covers/Cover2.jpeg" },
-    { songName:"Vaaroon - Mirzapur", filePath:"/songs/Song3.mp3", coverPath:"/covers/Cover3.jpeg" },
-    { songName:"Ishq Jalakar - Karvaan", filePath:"/songs/Song4.mp3", coverPath:"/covers/Cover4.webp" },
-    { songName:"Ishq Di Baajiyaan", filePath:"/songs/Song5.mp3", coverPath:"/covers/Cover5.jpeg" },
-    { songName:"Apna Bana Le", filePath:"/songs/Song6.mp3", coverPath:"/covers/Cover6.jpeg" },
-    { songName:"Sundari", filePath:"/songs/Song7.mp3", coverPath:"/covers/Cover7.webp" },
-    { songName:"Tum ho to", filePath:"/songs/Song8.mp3", coverPath:"/covers/Cover8.jpeg" },
+    { songName:"Shri Hanuman Chalisa", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover1.jpeg" },
+    { songName:"Dhrundhar Title Song", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover2.jpeg" },
+    { songName:"Vaaroon - Mirzapur", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover3.jpeg" },
+    { songName:"Ishq Jalakar - Karvaan", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover4.webp" },
+    { songName:"Ishq Di Baajiyaan", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover5.jpeg" },
+    { songName:"Apna Bana Le", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover6.jpeg" },
+    { songName:"Sundari", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover7.webp" },
+    { songName:"Tum ho to", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover8.jpeg" },
 ];
