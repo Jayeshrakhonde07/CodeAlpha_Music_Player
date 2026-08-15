b4c7a9bbddbe0a8060d794203f53333e8d7ff5e3
Jayeshrakhonde07 <jayeshrakhonde05@gmail.com>
2026-08-15 18:54:44 +0530

    correct song path

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let songIndex = 0;
 
 let songs = [
     { songName:"Shri Hanuman Chalisa", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover1.jpeg" },
-    { songName:"Dhrundhar Title Song", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover2.jpeg" },
-    { songName:"Vaaroon - Mirzapur", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover3.jpeg" },
-    { songName:"Ishq Jalakar - Karvaan", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover4.webp" },
-    { songName:"Ishq Di Baajiyaan", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover5.jpeg" },
-    { songName:"Apna Bana Le", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover6.jpeg" },
-    { songName:"Sundari", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover7.webp" },
-    { songName:"Tum ho to", filePath:"/Songs/Song1.mp3", coverPath:"/Images/Cover8.jpeg" },
+    { songName:"Dhrundhar Title Song", filePath:"/Songs/Song2.mp3", coverPath:"/Images/Cover2.jpeg" },
+    { songName:"Vaaroon - Mirzapur", filePath:"/Songs/Song3.mp3", coverPath:"/Images/Cover3.jpeg" },
+    { songName:"Ishq Jalakar - Karvaan", filePath:"/Songs/Song4.mp3", coverPath:"/Images/Cover4.webp" },
+    { songName:"Ishq Di Baajiyaan", filePath:"/Songs/Song5.mp3", coverPath:"/Images/Cover5.jpeg" },
+    { songName:"Apna Bana Le", filePath:"/Songs/Song6.mp3", coverPath:"/Images/Cover6.jpeg" },
+    { songName:"Sundari", filePath:"/Songs/Song7.mp3", coverPath:"/Images/Cover7.webp" },
+    { songName:"Tum ho to", filePath:"/Songs/Song8.mp3", coverPath:"/Images/Cover8.jpeg" },
 ];
 
 // DOM Elements
@@ -26,6 +26,13 @@ let gif = document.getElementById("gif");
 let masterName = document.getElementsByClassName("song-master-title");
 
 
+// Calculating Song Length
+function songDuration(duration) {
+    let minutes = Math.floor(duration / 60);
+    let seconds = Math.floor(duration % 60);
+    if (seconds < 10) seconds = "0" + seconds;
+    return `${minutes}:${seconds}`;
+}
 
 
 // Access Song UI
