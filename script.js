a7ba97d618de1c4cd783eaec3aa8d29941b08560
Jayeshrakhonde07 <jayeshrakhonde05@gmail.com>
2026-08-15 13:25:46 +0530

    add songs

---
+++ script.js
@@ -0,0 +1,12 @@
+let songIndex = 0;
+
+let songs = [
+    { songName:"Shri Hanuman Chalisa", filePath:"/songs/Song1.mp3", coverPath:"/covers/Cover1.jpeg" },
+    { songName:"Dhrundhar Title Song", filePath:"/songs/Song2.mp3", coverPath:"/covers/Cover2.jpeg" },
+    { songName:"Vaaroon - Mirzapur", filePath:"/songs/Song3.mp3", coverPath:"/covers/Cover3.jpeg" },
+    { songName:"Ishq Jalakar - Karvaan", filePath:"/songs/Song4.mp3", coverPath:"/covers/Cover4.webp" },
+    { songName:"Ishq Di Baajiyaan", filePath:"/songs/Song5.mp3", coverPath:"/covers/Cover5.jpeg" },
+    { songName:"Apna Bana Le", filePath:"/songs/Song6.mp3", coverPath:"/covers/Cover6.jpeg" },
+    { songName:"Sundari", filePath:"/songs/Song7.mp3", coverPath:"/covers/Cover7.webp" },
+    { songName:"Tum ho to", filePath:"/songs/Song8.mp3", coverPath:"/covers/Cover8.jpeg" },
+];
